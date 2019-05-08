3158e36d0573b915c901713aa670f3639638d3ac
Paul <paulusman@live.nl>
2019-05-08 11:17:59 +0200

    Created spring server

---
+++ WordWarServer/src/main/java/com/wordwargroup/wordwarserver/WordwarserverApplication.java
@@ -0,0 +1,13 @@
+package com.wordwargroup.wordwarserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordwarserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordwarserverApplication.class, args);
+    }
+
+}
---
+++ WordWarServer/src/test/java/com/wordwargroup/wordwarserver/WordwarserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wordwargroup.wordwarserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordwarserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
