d3fa3ae3c318c1acd42b69c0753f7548c6196c07
Paul <paulusman@live.nl>
2019-06-18 21:58:36 +0200

    Empty field check

--- WordWarClient/src/main/java/Client/GUIControllers/RegisterController.java
+++ WordWarClient/src/main/java/Client/GUIControllers/RegisterController.java
@@ -21,6 +21,19 @@ public class RegisterController implements IGUIController {
         String password = Textfield_Password.getText();
 
         if(password.compareTo(Textfield_Password_Confirm.getText()) == 0) {
+            register(username, password);
+        } else {
+                Label_Error.setText("Passwords do not match");
+            }
+    }
+
+    private void register(String username, String password) {
+
+        if(username.isEmpty() || password.isEmpty()) {
+            Label_Error.setText("Please fill in all fields");
+            Label_Error.setTextFill(Color.valueOf("Red"));
+            return;
+        }
             boolean success = logic.register(username, password);
             if(success) {
                 Label_Error.setText("User successfully created!");
@@ -29,9 +42,7 @@ public class RegisterController implements IGUIController {
                 Label_Error.setText("Username already taken.");
                 Label_Error.setTextFill(Color.valueOf("Red"));
             }
-        }else {
-            Label_Error.setText("Passwords do not match");
-        }
+
     }
 
     @Override
